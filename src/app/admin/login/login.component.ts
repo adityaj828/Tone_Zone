5919bceafa8a916d64ddbdb66ef976feb3dfbb71
Jash Doshi <jashdoshi24@gmail.com>
2022-02-11 18:16:34 +0530

    refactor: login code

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+import { AuthenticationService } from 'src/app/service/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,18 +10,50 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  loginForm = new FormGroup({
-    username: new FormControl(""),
-    password: new FormControl(""),
-  });
-  constructor() { }
+  loginForm: FormGroup;
+  loginFormSubmitted: boolean;
+  isLoading: boolean;
+  wrongAuthentication: boolean;
+  constructor(private authenticationService: AuthenticationService,
+              private router: Router) {
+    this.loginFormSubmitted = false;
+    this.wrongAuthentication = false;
+  }
 
   ngOnInit() {
-    
+    this.loginForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    });
   }
-  login(data){
-    alert(1);
-    console.log(data);
+
+  login() {
+    this.loginFormSubmitted = true;
+    this.isLoading = true;
+    if (this.loginForm.valid) {
+      this.authenticationService.login(this.username.value, this.password.value)
+        .pipe(first())
+        .subscribe(
+          data => {
+            if (data) {
+              this.wrongAuthentication = false;
+              this.router.navigate(['/dashboard']);
+            } else {
+              this.wrongAuthentication = true;
+              console.log(data);
+            }
+          },
+          error => {
+            console.error(error);
+            this.isLoading = false;
+          });
+    }
   }
-  
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
 }
