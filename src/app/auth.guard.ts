641f973acb519fe5001ab4aa61c4a1fa6a932061
Jash Doshi <jashdoshi24@gmail.com>
2022-02-13 18:36:46 +0530

    refactor: created auth guard

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,31 @@
+import { Location } from '@angular/common';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from './service/authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthenticationService, private router: Router, private location: Location) {
+
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if (this.location.path() === '/admin/login' && (!this.authService.isLoggedIn())) {
+        return true;
+      }
+      if (!this.authService.isLoggedIn()) {
+        this.router.navigate(['/admin/login']);
+        return false;
+      }
+      if (this.location.path() === '/admin/login') {
+        this.router.navigate(['/dashboard']);
+      }
+      return true;
+  }
+
+}
