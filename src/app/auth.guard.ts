ceeb0abde336a926121c877b01f4c6b973366918
Jash Doshi <jashdoshi24@gmail.com>
2022-02-21 12:36:58 +0530

    refactor: no change

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -15,6 +15,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
       if (this.location.path() === '/admin/login' && (!this.authService.isLoggedIn())) {
         return true;
       }
