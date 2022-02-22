10c42bc22bbab5dffd8145de469e16d20b8ca922
Jash Doshi <jashdoshi24@gmail.com>
2022-02-22 16:18:55 +0530

    refactor: ajax for user-view datatable

--- src/app/admin/user-view/user-view.component.ts
+++ src/app/admin/user-view/user-view.component.ts
@@ -1,4 +1,6 @@
+import { HttpParams } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { BaseService } from 'src/app/service/base.service';
 
 @Component({
   selector: 'app-user-view',
@@ -6,9 +8,46 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./user-view.component.scss']
 })
 export class UserViewComponent {
-  constructor() { }
+  users: [];
+  getUsersURL = 'users/all';
+  dtOptions: DataTables.Settings = {};
+
+  constructor(private baseService: BaseService) { }
 
   ngOnInit(): void {
+
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 5,
+      lengthMenu : [5, 10, 25],
+      serverSide: true,
+      processing: true,
+      ajax: (dataTablesParameters: any, callback) => {
+        const params = this.objectToHttpParams(dataTablesParameters);
+        console.log('params', params);
+
+        this.baseService.get(this.getUsersURL)
+          .subscribe(resp => {
+
+            this.users = resp;
+
+            callback({
+              recordsTotal: resp.length,
+              recordsFiltered: resp.length,
+              data: []
+            });
+          });
+      }
+    };
+  }
+
+  objectToHttpParams(obj: any) {
+    return Object.entries(obj || {}).reduce((params, [key, value]) => {
+      return params.set(
+        key,
+        JSON.stringify(value)
+      );
+    }, new HttpParams());
   }
 
 }
