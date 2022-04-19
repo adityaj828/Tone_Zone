9988c72b38ff6c6e0f15d4287986b74659041a20
SRIJANI181 <66792807+SRIJANI181@users.noreply.github.com>
2022-04-19 09:56:40 +0530

    Add files via upload

---
+++ src/app/card-service.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardServiceService {
+
+
+  constructor(private http: HttpClient) {} 
+  API='http://localhost:8081';
+
+  public getServiceCenter()
+  {
+    return this.http.get(this.API+'/service-centers/all');
+  }
+
+  ngOnInit(): void {
+  }
+}
